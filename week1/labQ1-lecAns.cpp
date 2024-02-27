4c5094ee3052c7bc4b419fb2a175d8d3d81aa15d
caizhen <zhenchai0000@gmail.com>
2024-02-27 10:22:12 +0800

    fix: try use normal run to execute the code without cout bug

--- week1/labQ1-lecAns.cpp
+++ week1/labQ1-lecAns.cpp
@@ -5,36 +5,32 @@ using namespace std;
 
 int main()
 {
-    string name;
-    string gender;
-    int age;
-    string address;
-    double ewallet_amount;
-
-    cout << "Student Name \t\t:";
+    string name; string gender; int age; string address; double ewallet_amount;
+    cout << "Student Name\t\t:";
     getline(cin, name);
-    cout << "Student Gender \t\t:";
+    cout << "Student Gender\t\t:";
     cin >> gender;
-    cout << "Student Age \t\t:";
+    cout << "Student Age\t\t:";
     cin >> age;
     cin.ignore();
-    cout << "Student Address \t:";
+    cout << "Student Home Address\t:";
     getline(cin, address);
-    cout << "E-wallet Amount \t:RM ";
+    cout << "E-wallet Amount\t\t:RM ";
     cin >> ewallet_amount;
+    cin.ignore();
 
-    cout << '\n' << "Press the Enter key to continue...";
-    cin.ignore(); // clear the buffer of the cin
-    while (cin.get() != '\n');
+    do {
+        cout << '\n' << "Press the Enter key to continue.";
+    } while (cin.get() != '\n');
 
-    cout << endl
-         << "Student details as below: \n";
-    cout << string(81, '=') << '\n';
+    system("cls"); // windows env use cls 
+    system("clear"); // unix env use clear 
+    cout << endl << "Student details as below: \n";
+    cout << string(81, '=') << endl;
     cout << "|Name \t\t | Age \t | Gender \t| Address \t| E-wallet amount\t|\n";
-    cout << string(81, '=') << '\n';
+    cout << string(81, '=') << endl;
     cout << "|" << name << " \t | " << age << " \t | " << gender << " \t| " << address << " \t| RM";
     cout << fixed << setprecision(2) << ewallet_amount << "\t\t|\n";
     cout << string(81, '=') << endl;
-
     return 0;
 }
